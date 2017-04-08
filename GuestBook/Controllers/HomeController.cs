9cfef84852db7a1b8767d44b5cdc039e529fca79
Ody Mbegbu <odytrice@gmail.com>
2017-04-08 17:44:01 +0100

    Added Bootstrap

--- GuestBook/Controllers/HomeController.cs
+++ GuestBook/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace GuestBook.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return RedirectToAction("index", "event");
+            return View();
         }
     }
 }
---
+++ GuestBook/Scripts/main.js
@@ -0,0 +1,1 @@
+﻿$("[date-picker]").datepicker();
